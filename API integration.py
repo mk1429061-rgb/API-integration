c4f4920d0eb4233ea5666dcc02c261056d201045
mk1429061-rgb <mk1429061@gmail.com>
2026-06-26 18:54:24 +0530

    Add files via upload

---
+++ API integration.py
@@ -0,0 +1,63 @@
+Python 3.14.2 (tags/v3.14.2:df79316, Dec  5 2025, 17:18:21) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+
+=================================================== RESTART: C:\Users\Haris\AppData\Local\Programs\python\Python314\API integration.py ==================================================
+============================================================
+USER DETAILS
+============================================================
+Name     : Leanne Graham
+Username : Bret
+Email    : Sincere@april.biz
+City     : Gwenborough
+--------------------------------------------------
+Name     : Ervin Howell
+Username : Antonette
+Email    : Shanna@melissa.tv
+City     : Wisokyburgh
+--------------------------------------------------
+Name     : Clementine Bauch
+Username : Samantha
+Email    : Nathan@yesenia.net
+City     : McKenziehaven
+--------------------------------------------------
+Name     : Patricia Lebsack
+Username : Karianne
+Email    : Julianne.OConner@kory.org
+City     : South Elvis
+--------------------------------------------------
+Name     : Chelsey Dietrich
+Username : Kamren
+Email    : Lucio_Hettinger@annie.ca
+City     : Roscoeview
+--------------------------------------------------
+Name     : Mrs. Dennis Schulist
+Username : Leopoldo_Corkery
+Email    : Karley_Dach@jasper.info
+City     : South Christy
+--------------------------------------------------
+Name     : Kurtis Weissnat
+Username : Elwyn.Skiles
+Email    : Telly.Hoeger@billy.biz
+City     : Howemouth
+--------------------------------------------------
+Name     : Nicholas Runolfsdottir V
+Username : Maxime_Nienow
+Email    : Sherwood@rosamond.me
+City     : Aliyaview
+--------------------------------------------------
+Name     : Glenna Reichert
+Username : Delphine
+Email    : Chaim_McDermott@dana.io
+City     : Bartholomebury
+--------------------------------------------------
+Name     : Clementina DuBuque
+Username : Moriah.Stanton
+Email    : Rey.Padberg@karina.biz
+City     : Lebsackbury
+--------------------------------------------------
+
+Enter city name to search users: South Christy
+
+Users Found:
+
+Mrs. Dennis Schulist (Karley_Dach@jasper.info)
